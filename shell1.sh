016d18370545a24846c2bc688ddb22d5cd1d4136
ahmet <ahoz1999@gmail.com>
2023-03-22 16:53:29 +0300

    klasör oluşturuldu

---
+++ shell1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sayac=4
+while [ $sayac -ne "0" ]
+do 
+ echo "Enter User Name: "
+ read name
+ echo "Enter Password: "
+ read pass
+ if [ $pass = "password" ] && [ $name = "ahmet" ]
+ then
+  sleep 1
+  echo "Hi &name ! Welcome to Kabuk3"
+  echo "Actual Calender is loading.."
+  sleep 1
+  sh shell1.sh
+  break
+ else
+  sayac=$(($sayac-1))
+  sleep 1
+  echo "Şifre hatalı! Tekrar deneyiniz."
+  echo "Kalan Deneme Hakkınız: $sayac"
+  sleep 1
+ fi
+done
