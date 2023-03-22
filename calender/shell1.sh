7fa2eb9e7148af6ee638828883cd1a2517428835
ahmet <ahoz1999@gmail.com>
2023-03-22 18:38:14 +0300

    shell1 dosyasındaki hatalar düzeltildi

--- calender/shell1.sh
+++ calender/shell1.sh
@@ -10,10 +10,10 @@ do
  if [ $pass = "password" ] && [ $name = "ahmet" ]
  then
   sleep 1
-  echo "Hi &name ! Welcome to Kabuk3"
+  echo "Hi $name ! Welcome to Kabuk3"
   echo "Actual Calender is loading.."
   sleep 1
-  sh shell1.sh
+  sh shell2.sh
   break
  else
   sayac=$(($sayac-1))
